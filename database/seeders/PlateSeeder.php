38587a412e99990885ea62b824306e7a45813e91
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2023-02-06 10:54:56 +0100

    added seeders

--- database/seeders/PlateSeeder.php
+++ database/seeders/PlateSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Plate;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class PlateSeeder extends Seeder
 {
@@ -12,8 +15,19 @@ class PlateSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $plates = config('db.plates');
+        foreach ($plates as $plate) {
+            $new_plate = new Plate();
+            $new_plate->restaurant_id = $plate['restaurant_id'];
+            $new_plate->name = $plate['name'];
+            $new_plate->slug = Str::slug($plate['name']);
+            $new_plate->description = $plate['description'];
+            $new_plate->price = $faker->randomFloat(2, 5, 15);
+            $new_plate->visibility = true;
+            // $new_plate->image
+            $new_plate->save();
+        }
     }
 }
