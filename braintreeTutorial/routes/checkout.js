4ed0fd67de67f4ebf26d6aa34724974254156a3c
macchiagabriele13 <macchia_gabriele@libero.it>
2023-02-14 18:01:27 +0100

    modifiche test

---
+++ braintreeTutorial/routes/checkout.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+    const gateway = new braintree.BraintreeGateway({
+        environment: braintree.Environment.Sandbox,
+        // Use your own credentials from the sandbox Control Panel here
+        merchantId: 'xhh6g83dj5p3g5t4',
+        publicKey: 'rbrb7sfvkp625hcp',
+        privateKey: '6e87f5de40b02a8e4fc6791b5a9fca01'
+    });
+
+    // Use the payment method nonce here
+    const nonceFromTheClient = req.body.paymentMethodNonce;
+    // Create a new transaction for $10
+    const newTransaction = gateway.transaction.sale({
+        amount: '10.00',
+        paymentMethodNonce: nonceFromTheClient,
+        options: {
+            // This option requests the funds from the transaction
+            // once it has been authorized successfully
+            submitForSettlement: true
+        }
+    }, (error, result) => {
+        if (result) {
+            res.send(result);
+        } else {
+            res.status(500).send(error);
+        }
+    });
+});
+
+module.exports = router;
