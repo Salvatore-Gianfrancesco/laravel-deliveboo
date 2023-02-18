04e6038cb97df7e1ba98739252a6590d321c5446
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2023-02-18 11:00:15 +0100

    fixed welcome image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,17 @@
 @section('content')
     <div class="jumbotron p-5 mb-4 rounded-3">
         <div class="container py-5">
-            
+
             <h1 class="display-5 fw-bold pb-2">
-               Benvenuto nel tuo Gestionale
+                Benvenuto nel tuo Gestionale
             </h1>
 
             <p class="col-md-8 fs-4">Una piattaforma pensata, realizzata e studiata per i nostri clienti.</p>
             <p>Per qualsiasi problema puoi rivolgerti al nostro servizio clienti, al numero: 02-3478931</p>
 
             <div class="logo_laravel">
-                <img width="150" height="150" src="{{asset('storage/img/pacfood-logo.png')}}" alt="">
+                <img width="150" height="150" src="{{ asset('storage/img/pacfood-logo.png') }}" alt="">
             </div>
         </div>
     </div>
-
 @endsection
